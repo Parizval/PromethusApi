d7e649c95dd4963aa718c66ec762e6c7c5d380f2
Anmol Goyal <anmolgoyal@gmail.com>
2020-08-04 00:36:49 +0530

    Added Post Route

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
+import process
+
 app = Flask(__name__, static_url_path='/static')
 
 app.debug = True
@@ -7,7 +9,17 @@ app.secret_key = "Nothing"
 
 @app.route('/')
 def Home():
-   return render_template("index.html")
 
+    data = process.RenderTable()
+    return render_template("index.html",data=data)
+
+@app.route('/request', methods=['POST'])
+def GetData():
+    data = request.form['name']
+    print(data)
+    reponse = {}
+    reponse['check'] = True
+    return reponse 
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
