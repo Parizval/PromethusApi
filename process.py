15d44f2adce6126354f2ba02e95315428341bce2
Anmol Goyal <anmolgoyal@gmail.com>
2020-08-04 16:18:16 +0530

    Modified Code with uuid data referencing

--- process.py
+++ process.py
@@ -1,13 +1,30 @@
+import uuid 
 
+
+field_map = {}
+value_map = {}
 
 def RenderTable():
     file = open("data.txt",'r')
+    
+    global field_map 
+    global value_map 
 
     lines = file.readlines()
     data = {}
     counter = 0 
     for  x  in lines:
+
         key,val = x.split()
-        data[counter] = {"field":key,"value":val} 
-        counter +=1 
+        number = uuid.uuid4()        
+        
+        data[counter] = {"field":key,"value":val,"id":number}
+
+        field_map[number] = key 
+        value_map[number] = val 
+        
+        counter +=1
+
     return data
+
+RenderTable()
